c32061c92b3e35b12130da83722a6646dbdbf2c3
unknown <yoanka97@gmail.com>
2022-05-04 11:51:54 +0200

    implement actions

--- document.py
+++ document.py
@@ -1,8 +1,11 @@
 from random import randrange
+from typing import Union
+
 import numpy as np
 from action import Action
-from simObj import Agent, Chair, Table
+from simObj import Agent, Chair, Table, ISimObj
 from cellCodes import CellCodes
+
 
 class Document:
     """
@@ -13,46 +16,106 @@ class Document:
         self.agents = list()
         self.tables = list()
         self.chairs = list()
-        self.grid = [[None for x in range(self.size[1])] for y in range(self.size[0])] 
+        self.grid: list[list[Union[None, ISimObj]]] = [[None for x in range(self.size[1])] for y in range(self.size[0])]
 
-    # returns true if action was valid and was performed, false otherwise 
-    def ApplyActionToAgent(self, agent: Agent, action : Action) -> bool:
+    # returns true if action was valid and was performed, false otherwise
+    def applyActionToAgent(self, agent: Agent, action: Union[Action, None]) -> bool:
         inGridAgent = self.grid[agent.x][agent.y]
         assert(inGridAgent == agent)
 
-        ##############################################
-        # TEMP
-        Xmove = randrange(-1,2,1)
-        Ymove = randrange(-1,2,1)
-        XNew = agent.x + Xmove
-        YNew = agent.y + Ymove
+        newPosition = np.array([agent.x, agent.y])
 
-        if(XNew < 0): XNew = 0
-        if(XNew >= self.size[0]): XNew = self.size[0]-1
-        if(YNew < 0): YNew = 0
-        if(YNew >= self.size[1]): YNew = self.size[1]-1
-        self.grid[agent.x][agent.y] = None
-        self.grid[XNew][YNew] = agent
-        agent.x = XNew
-        agent.y = YNew
+        # current position of grabbed object
+        if agent.grab:
+            targetPosition = newPosition + Action.directionToVector(agent.grab)
+        else:
+            targetPosition = np.array([None, None])
 
-        agent.SetLastAction(action) # run only if true
+        if action is None:
+            # random movement
+            Xmove = randrange(-1, 2, 1)
+            Ymove = randrange(-1, 2, 1)
+            newPosition += [Xmove, Ymove]
+        else:
+            # carry out action
+            if action.type == Action.release:
+                agent.grab = None
+
+            elif action.type == Action.move:
+                # move to a different square
+                newPosition += Action.directionToVector(action.direction)
+
+            elif action.type == Action.grab:
+                # grab furniture
+                if agent.grab:
+                    # ignore grab actions while holding an object
+                    return False
+
+                targetPosition = newPosition + Action.directionToVector(action.direction)
+
+                # check if there's furniture there
+                grabTarget = self.grid[targetPosition[0]][targetPosition[1]]
+                if self.GetCellCode(targetPosition[0], targetPosition[1]) in [CellCodes.Chair, CellCodes.Table_max, CellCodes.Table_min]:
+                    # TODO: consider legal table-carrying configurations (can you only carry by holding short ends?)
+                    agent.grab = action.direction
+
+            elif action.type == Action.wait:
+                return True
+
+        # check against walking off-scene
+        if newPosition[0] < 0:
+            newPosition[0] = 0
+        if newPosition[0] >= self.size[0]:
+            newPosition[0] = self.size[0]-1
+        if newPosition[1] < 0:
+            newPosition[1] = 0
+        if newPosition[1] >= self.size[1]:
+            newPosition[1] = self.size[1]-1
+
+        # check against walking onto occupied cells - can only push the grabbed object or move onto empty cells
+        if not np.array_equal(newPosition, targetPosition) and \
+                self.GetCellCode(newPosition[0], newPosition[1]) != CellCodes.Empty:
+            return False
+
+        # TODO: check against moving furniture off-scene or onto occupied cells
+        # TODO: check if a table can be moved
+        #  (agents can check for intentions to move other side or submit their own intention for next agents in line?)
+        if agent.grab and action.type == Action.move:
+            pass
+
+        # TODO: move tables
+        if agent.grab and action.type == Action.move:
+            target = self.grid[targetPosition[0]][targetPosition[1]]
+            targetCode = self.GetCellCode(targetPosition[0], targetPosition[1])
+            targetNewPosition = targetPosition + Action.directionToVector(action.direction)
+            if targetCode == CellCodes.Chair:
+                target.x = targetPosition[0]
+                target.y = targetPosition[1]
+                self.grid[targetPosition[0]][targetPosition[1]] = None
+                self.grid[targetNewPosition[0]][targetNewPosition[1]] = target
+
+        if self.grid[agent.x][agent.y] == agent:
+            # replace cell with empty if no furniture was just placed there
+            self.grid[agent.x][agent.y] = None
+        self.grid[newPosition[0]][newPosition[1]] = agent
+
+        agent.x = newPosition[0]
+        agent.y = newPosition[1]
+
+        # agent.SetLastAction(action) # run only if true
         return True
-        # TEMP
-        ##############################################
-
 
     # returns cell code for given x,y from CellCodes list
     def GetCellCode(self, x: int, y: int) -> int:
-        if(x < 0 or y < 0 or x >= self.size[0] or y >= self.size[1]):
+        if x < 0 or y < 0 or x >= self.size[0] or y >= self.size[1]:
             return CellCodes.Inaccessible
         cellObj = self.grid[x][y]
-        if(cellObj == None):
+        if cellObj is None:
             return CellCodes.Empty
         return cellObj.GetCode(x, y)
 
     def AddAgent(self, x: int, y: int) -> bool:
-        if(self.grid[x][y] != None):
+        if self.grid[x][y] is not None:
             return False
 
         agent = Agent(x, y)
@@ -60,13 +123,12 @@ class Document:
         self.agents.append(agent)
         return True
 
-
     def AddTable(self, x1: int, y1: int, x2: int, y2: int) -> bool:
-        if(self.grid[x1][y1] != None):
+        if self.grid[x1][y1] is not None:
             return False
-        if(self.grid[x2][y2] != None):
+        if self.grid[x2][y2] is not None:
             return False
-        if(abs(x1-x2) + abs(y1-y2) != 1):
+        if (abs(x1-x2) + abs(y1-y2)) != 1:
             return False
 
         table = Table(x1, y1, x2, y2)
@@ -75,9 +137,8 @@ class Document:
         self.tables.append(table)
         return True
 
-
     def AddChair(self, x: int, y: int) -> bool:
-        if(self.grid[x][y] != None):
+        if self.grid[x][y] is not None:
             return False
 
         chair = Chair(x, y)
--- screen.py
+++ screen.py
@@ -8,12 +8,13 @@ from document import Document
 from simObj import Agent
 from action import Action
 
-FPS = 30 #frames per second setting
+FPS = 30  # frames per second setting
 white = (255, 255, 255)
 black = (  0,   0,   0)
-green = (0, 255, 0)
-blue = (0, 0, 180)
+green = (  0, 255,   0)
+blue  = (  0,   0, 180)
 red   = (255,   0,   0)
+
 
 class Screen:
     """
@@ -33,10 +34,10 @@ class Screen:
         pygame.display.set_caption('Interactive agents') # Name for the window
         self.screen.fill(background_colour) #This syntax fills the background colour
 
-    def Update(self, doc : Document) -> None:
+    def update(self, doc : Document) -> None:
         self.screen.fill(white)
 
-        self.DrawDoc(doc)
+        self.drawDoc(doc)
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -46,27 +47,26 @@ class Screen:
         pygame.display.update()
         self.fpsClock.tick(FPS)
 
-
-    def DrawDoc(self, doc : Document):
-        (screenX, screenY) = self.GetScreenSize()
+    def drawDoc(self, doc: Document):
+        (screenX, screenY) = self.getScreenSize()
         (cellsX, cellsY) = doc.size
         cellSize = int(min(screenX / cellsX, screenY / cellsY))
-        boardRect = Rect(0,0, cellSize * cellsX, cellSize * cellsY)
+        boardRect = Rect(0, 0, cellSize * cellsX, cellSize * cellsY)
 
-        self.DrawRect(boardRect, (200,200,255))
+        self.drawRect(boardRect, (200, 200, 255))
 
-        self.DrawCellLines(boardRect, doc)
+        self.drawCellLines(boardRect, doc)
         for agent in doc.agents:
-            self.DrawAgent(agent, boardRect, doc)
+            self.drawAgent(agent, boardRect, doc)
+            print("agent x: ", agent.x, " y: ", agent.y)
 
         for table in doc.tables:
-            self.DrawTable(table.x1, table.y1, table.x2, table.y2, boardRect, doc)
+            self.drawTable(table.x1, table.y1, table.x2, table.y2, boardRect, doc)
 
         for chair in doc.chairs:
-            self.DrawChair(chair.x, chair.y, boardRect, doc)
+            self.drawChair(chair.x, chair.y, boardRect, doc)
 
-
-    def DrawCellLines(self, boardRect : Rect, doc : Document):
+    def drawCellLines(self, boardRect: Rect, doc: Document):
         (cellsX, cellsY) = doc.size
         assert int(boardRect.width / cellsX) == int(boardRect.height / cellsY)
 
@@ -74,13 +74,14 @@ class Screen:
         lineWidth = 1
         
         for x in range(0, cellsX+1):
-            pygame.draw.line(self.screen, black, (boardRect.x + x * cellSize, boardRect.y), (boardRect.x + x * cellSize, boardRect.y + cellsY * cellSize), lineWidth)
+            pygame.draw.line(self.screen, black, (boardRect.x + x * cellSize, boardRect.y),
+                             (boardRect.x + x * cellSize, boardRect.y + cellsY * cellSize), lineWidth)
         for y in range(0, cellsY+1):
-            pygame.draw.line(self.screen, black, (boardRect.x, boardRect.y + y * cellSize), (boardRect.x + cellsX * cellSize, boardRect.y + y * cellSize), lineWidth)
-
-        
+            pygame.draw.line(self.screen, black, (boardRect.x, boardRect.y + y * cellSize),
+                             (boardRect.x + cellsX * cellSize, boardRect.y + y * cellSize), lineWidth)
     
-    def DrawAgent(self, agent: Agent, boardRect : Rect, doc : Document) -> None:
+
+    def drawAgent(self, agent: Agent, boardRect : Rect, doc : Document) -> None:
         (cellsX, cellsY) = doc.size
         assert int(boardRect.width / cellsX) == int(boardRect.height / cellsY)
         cellSize = int(min(boardRect.width / cellsX, boardRect.height / cellsY))
@@ -89,58 +90,54 @@ class Screen:
         Y = cellsY - Y -1   # Y flip
 
         agentRect = Rect(boardRect.x + X * cellSize, boardRect.y + Y * cellSize, cellSize, cellSize)
-        self.DrawAgentInRect(agentRect)
+        self.drawAgentInRect(agentRect)
 
-        last_action = agent.last_action
-        if(last_action is not None and last_action.type == Action.grab):
+        # last_action = agent.last_action
+        if(agent.grab):
             sX = agentRect.centerx
             sY = agentRect.centery
-            if(last_action.direction == Action.north):
+            if(agent.grab == Action.north):
                 grabLineEnd = [sX,sY - cellSize]
-            elif(last_action.direction == Action.east):
+            elif(agent.grab == Action.east):
                 grabLineEnd = [sX + cellSize, sY]
-            elif(last_action.direction == Action.south):
+            elif(agent.grab == Action.south):
                 grabLineEnd = [sX,sY + cellSize]
-            elif(last_action.direction == Action.west):
+            elif(agent.grab == Action.west):
                 grabLineEnd = [sX - cellSize, sY]
             
-            self.DrawAgentGrab([sX,sY], grabLineEnd);
+            self.drawAgentGrab([sX,sY], grabLineEnd);
 
 
-    def DrawTable(self, X1 : int, Y1 : int, X2 : int, Y2 : int, boardRect : Rect, doc : Document) -> None:
+    def drawTable(self, X1: int, Y1: int, X2: int, Y2: int, boardRect: Rect, doc: Document) -> None:
         (cellsX, cellsY) = doc.size
         assert int(boardRect.width / cellsX) == int(boardRect.height / cellsY)
         cellSize = int(min(boardRect.width / cellsX, boardRect.height / cellsY))
         Xmin = min(X1, X2)
         Ymin = max(Y1, Y2)
-        Ymin = cellsY - Ymin -1   # Y flip
-        if(X1 != X2):
+        Ymin = cellsY - Ymin - 1   # Y flip
+        if X1 != X2:
             tableRect = Rect(boardRect.x + Xmin * cellSize, boardRect.y + Ymin * cellSize, cellSize * 2 , cellSize)
         else:
             tableRect = Rect(boardRect.x + Xmin * cellSize, boardRect.y + Ymin * cellSize, cellSize, cellSize * 2)
 
-        self.DrawTableInRect(tableRect)
+        self.drawTableInRect(tableRect)
 
-
-    def DrawChair(self, X : int, Y : int, boardRect : Rect, doc : Document) -> None:
+    def drawChair(self, X: int, Y: int, boardRect: Rect, doc: Document) -> None:
         (cellsX, cellsY) = doc.size
         assert int(boardRect.width / cellsX) == int(boardRect.height / cellsY)
         cellSize = int(min(boardRect.width / cellsX, boardRect.height / cellsY))
         Y = cellsY - Y -1   # Y flip
 
         chairRect = Rect(boardRect.x + X * cellSize, boardRect.y + Y * cellSize, cellSize, cellSize)
-        self.DrawChairInRect(chairRect)
+        self.drawChairInRect(chairRect)
 
-
-    def GetScreenSize(self) -> Tuple[int, int]:
+    def getScreenSize(self) -> Tuple[int, int]:
         return self.screen.get_size()
         
-
-    def DrawRect(self, rect : Rect, color) -> None:
+    def drawRect(self, rect : Rect, color) -> None:
         pygame.draw.rect(self.screen, color, rect)
 
-
-    def DrawAgentInRect(self, rect : Rect) -> None:
+    def drawAgentInRect(self, rect : Rect) -> None:
         off = int(rect.width / 10)
         topCenterX = (rect.left + rect.right) / 2
         points = (
@@ -151,21 +148,18 @@ class Screen:
         agentColor = (255, 0, 0)
         pygame.draw.polygon(self.screen, agentColor, points)
 
-    def DrawAgentGrab(self, start, end) -> None:
+    def drawAgentGrab(self, start, end) -> None:
         agentColor = (255, 150, 0)
         pygame.draw.line(self.screen, agentColor, start, end, 3);
 
-
-    def DrawTableInRect(self, rect : Rect) -> None:
+    def drawTableInRect(self, rect : Rect) -> None:
         deflate = -0.1
         rect = rect.inflate(deflate * rect.width, deflate * rect.height)
         tableColor = (180, 20, 20)
         pygame.draw.ellipse(self.screen, tableColor, rect)
 
-
-    def DrawChairInRect(self, rect : Rect) -> None:
+    def drawChairInRect(self, rect : Rect) -> None:
         deflate = -0.4
         rect = rect.inflate(deflate * rect.width, deflate * rect.height)
         chairColor = (100, 0, 0)
         pygame.draw.ellipse(self.screen, chairColor, rect)
-
