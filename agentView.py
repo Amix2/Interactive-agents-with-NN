a975313f3de64223586c2d87844d1c4438efbc46
unknown <yoanka97@gmail.com>
2022-05-18 11:14:02 +0200

    basic env reward calculation and empty L function

--- agentView.py
+++ agentView.py
@@ -1,4 +1,9 @@
-from document import Document
+# following 3 lines are a workaround for avoiding circular deps with typing-related imports
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from document import Document
+
 
 class AgentView:
     """
@@ -8,21 +13,19 @@ class AgentView:
         self.size = size
         self.data = []
         d = int(size / 2)
-        for iy in range(-d,d+1):
-            for ix in range(-d,d+1):
-                self.data.append(doc.GetCellCode(x+ix, y+iy))
-    
+        for iy in range(-d, d+1):
+            for ix in range(-d, d+1):
+                self.data.append(doc.getCellCode(x + ix, y + iy))
 
-    def GetData(self, x:int, y:int) -> int:
+    def GetData(self, x: int, y: int) -> int:
         return self.data[x + y * self.size]
-
 
     def ToPrettyString(self) -> str:
         out = "=====\n"
-        for y in range(0,self.size):
+        for y in range(0, self.size):
             out += "[ "
-            for x in range(0,self.size):
-                out += str(self.GetData(x,y))
+            for x in range(0, self.size):
+                out += str(self.GetData(x, y))
             out += " ]\n"
         out += "=====\n"
         return out
